9640ca0ecef3f3da4e40f36f4d1aeef91957dfa5
SmartWalle <smartwalle@gmail.com>
2021-01-17 12:44:04 +0800

    time

--- delay.go
+++ delay.go
@@ -4,7 +4,8 @@ import "time"
 
 // Delay 延迟执行子行为。
 // 等待一定的时间之后执行子行为并返回子行为的执行结果，否则返回 Running。
-// 每一次执行完子行为之后，会重新计算下一次执行时间。
+// 子行为返回 Running 的时候，不会重新计算执行时间，下一次 Tick 会继续执行子行为。
+// 子行为返回 Success 或者 Failure 的时候，下一次开始执行子行为之前，会重新计算执行时间。
 type Delay struct {
 	Decorator
 	duration time.Duration
--- delay_test.go
+++ delay_test.go
@@ -18,7 +18,7 @@ func TestNewDelay(t *testing.T) {
 		t.Fatal("已过时间，延时任务结果不应该为 Running")
 	}
 
-	// 第二轮延时开始
+	// 第二轮延时开始，此次 Tick 会重新计算延时时间
 	status = n.Tick(nil)
 	if status != behavior.Running {
 		t.Fatal("未到时间，延时任务结果应该为 Running")
