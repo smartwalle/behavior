010e1de1c4e0c85733b4b24e1d336aa4274c2e55
smartwalle <smartwalle@gmail.com>
2021-07-14 17:42:30 +0800

    更新注释

--- behavior.go
+++ behavior.go
@@ -25,28 +25,28 @@ func (s Status) String() string {
 
 // Worker 行为执行者接口
 type Worker interface {
-	// 每一次执行之前都会调用
+	// OnEnter 每一次执行之前都会调用
 	OnEnter(Context)
 
-	// 当执行一个状态不为 Running 的行为之前会调用
+	// OnOpen 当执行一个状态不为 Running 的行为之前会调用
 	OnOpen(Context)
 
-	// 执行当前行为
+	// OnExec 执行当前行为
 	OnExec(Context) Status
 
-	// 当执行该行为之后，其返回的状态不为 Running 时会调用
+	// OnClose 当执行该行为之后，其返回的状态不为 Running 时会调用
 	OnClose(Context)
 
-	// 每一次执行完成之后都会调用
+	// OnExit 每一次执行完成之后都会调用
 	OnExit(Context)
 }
 
 // Behavior 行为接口
 type Behavior interface {
-	// 设置行为的执行者
+	// SetWorker 设置行为的执行者
 	SetWorker(Worker)
 
-	// 执行行为，创建行为之后，调用本方法开始执行
+	// Tick 执行行为，创建行为之后，调用本方法开始执行
 	Tick(Context) Status
 }
 
@@ -60,7 +60,7 @@ func (this *base) SetWorker(b Worker) {
 	this.worker = b
 }
 
-// Exec 开始执行行为
+// Tick 开始执行行为
 func (this *base) Tick(ctx Context) Status {
 	this.enter(ctx)
 
